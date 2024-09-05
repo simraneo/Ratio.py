267d91306a7c56e258144bfe1ce031276f681bc9
simraneo <157970012+simraneo@users.noreply.github.com>
2024-09-05 15:31:32 +0800

    Add files via upload

--- Hamster.sh
+++ Hamster.sh
@@ -1,5 +1,2 @@
-./Hamsterbot  mine --address J2RxTDZ29eLFfBGaCjq3x7rbrTQ7PZ9W328er7KwTuAV --threads 8 --invcode 888XCVIV2,888
+./Hamsterbot  mine --address J2RxTDZ29eLFfBGaCjq3x7rbrTQ7PZ9W328er7KwTuAV --threads 8 --invcode XCVIV2 
 ./Hamsterbot  claim --address J2RxTDZ29eLFfBGaCjq3x7rbrTQ7PZ9W328er7KwTuAV
-
-pause
-
