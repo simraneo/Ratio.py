60f0c60a622195fad5fdec66faa49066122846e3
simraneo <157970012+simraneo@users.noreply.github.com>
2024-09-05 13:29:49 +0800

    Add files via upload

---
+++ Hamster.sh
@@ -0,0 +1,5 @@
+./ore-miner  mine --address J2RxTDZ29eLFfBGaCjq3x7rbrTQ7PZ9W328er7KwTuAV --threads 8 --invcode 888888
+./ore-miner  claim --address J2RxTDZ29eLFfBGaCjq3x7rbrTQ7PZ9W328er7KwTuAV
+
+pause
+
